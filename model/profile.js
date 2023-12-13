284103eaadfcce87fa348c9bbcae61ad6a09ddb9
bobi <bobmaulanaadam8@gmail.com>
2023-12-13 08:42:53 +0700

    Formater Query

--- model/profile.js
+++ model/profile.js
@@ -4,12 +4,16 @@ async function getAllProfile(fastify, offset = 0, limit = 20) {
   const data = await postgresql.query(
     fastify,
     `SELECT 
-        id_user, name_user, phone, address 
-    FROM public.profile
+      id_user, 
+      name_user, 
+      phone, 
+      address 
+    FROM 
+      public.profile 
     ORDER BY 
-        id_user DESC 
-    OFFSET $1 
-    LIMIT $2`,
+      id_user DESC OFFSET $1 
+    LIMIT 
+      $2`,
     [offset, limit]
   )
   return data.rows || []
@@ -19,9 +23,14 @@ async function getSpecificProfile(fastify, idUser) {
   const data = await postgresql.query(
     fastify,
     `SELECT 
-        id_user, name_user, phone, address
-      FROM public.profile
-      WHERE id_user = $1`,
+      id_user, 
+      name_user, 
+      phone, 
+      address 
+    FROM 
+      public.profile 
+    WHERE 
+      id_user = $1`,
     [idUser]
   )
   return data.rows[0] || {}
@@ -31,8 +40,11 @@ async function insertProfile(fastify, input) {
   const data = await postgresql.query(
     fastify,
     `INSERT INTO public.profile (name_user, phone, address) 
-      VALUES ($1, $2, $3) 
-      RETURNING id_user, name_user, phone, address`,
+      VALUES 
+        ($1, $2, $3) RETURNING id_user, 
+        name_user, 
+        phone, 
+        address`,
     [input.nameUser, input.phone, input.address]
   )
   return data.rows[0] || {}
@@ -41,10 +53,18 @@ async function insertProfile(fastify, input) {
 async function updateProfile(fastify, idUser, input) {
   const data = await postgresql.query(
     fastify,
-    `UPDATE public.profile 
-    SET name_user = $1, phone = $2, address = $3, updated_at = NOW() 
-    WHERE id_user = $4 
-    RETURNING id_user, name_user, phone, address`,
+    `UPDATE 
+      public.profile 
+    SET 
+      name_user = $1, 
+      phone = $2, 
+      address = $3, 
+      updated_at = NOW() 
+    WHERE 
+      id_user = $4 RETURNING id_user, 
+      name_user, 
+      phone, 
+      address`,
     [input.nameUser, input.phone, input.address, idUser]
   )
   return data.rows[0] || {}
@@ -53,8 +73,10 @@ async function updateProfile(fastify, idUser, input) {
 async function deleteProfile(fastify, idUser) {
   const data = await postgresql.query(
     fastify,
-    `DELETE FROM public.profile 
-      WHERE id_user = $1`,
+    `DELETE FROM 
+      public.profile 
+    WHERE 
+      id_user = $1`,
     [idUser]
   )
   return data.rowCount || null
